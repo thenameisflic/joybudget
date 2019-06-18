2b6f51d990c2754e00ffcf8c52cb8d204d7c5221
Feliciano Santana <fsfelicianosantana@gmail.com>
2019-06-18 18:32:27 +0000

    Add sentry

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import "typeface-merriweather-sans";
 import "typeface-merriweather";
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
 
 import setupI18n from "./i18n";
 
 setupI18n(() => {
+  Sentry.init({dsn: "https://a4dcff62141c4c70afd862ee3fd60402@sentry.io/1485053"});
   ReactDOM.render(<App />, document.getElementById('root'));
 });
 
